4da77dfb5980b61eb181f63c5e9d188647e7acf8
João Marcos Figueira <joao.figueira@hbsis.com.br>
2019-08-06 17:27:28 -0300

    - Example.

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+
+namespace Azure_NETCORE_Example.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            string token = "";
+            if (Request.Headers.TryGetValue("User-Token", out StringValues headerValue))
+            {
+                token = headerValue.ToString();
+            }
+            return token;
+        }
+    }
+}
---
+++ Filters/AddRequiredHeaderParameter.cs
@@ -0,0 +1,30 @@
+﻿using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Azure_NETCORE_Example.Filters
+{
+    public class AddRequiredHeaderParameter : IOperationFilter
+    {
+        public void Apply(Operation operation, OperationFilterContext context)
+        {
+            if (operation.Parameters == null)
+                operation.Parameters = new List<IParameter>();
+
+            operation.Parameters.Add(new HeaderParameter()
+            {
+                Name = "User-Token",
+                In = "header",
+                Type = "string",
+                Required = false
+            });
+        }
+
+        class HeaderParameter : NonBodyParameter
+        {
+        }
+    }
+}
