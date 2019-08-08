eb1cf39da2c15e2d2042634ef9ac4edb43b0169d
João Marcos Figueira <joao.figueira@hbsis.com.br>
2019-08-08 14:28:13 -0300

    -Add (Cosumer/Job)

---
+++ Azure_NETCORE_Consumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Azure_NETCORE_Consumer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("My Consumer Starts and Completed...");
+        }
+    }
+}
---
+++ Azure_NETCORE_Job/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Azure_NETCORE_Job
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("My Job Starts and Completed...");
+        }
+    }
+}
